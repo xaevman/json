adae06dee0d63bedc07a83f7c94ff2f082ba6c3b
Jared Chavez <jared@undeadlabs.com>
2016-07-26 18:16:26 -0700

    return an error on parse failure

--- json.go
+++ json.go
@@ -2,7 +2,7 @@
 //
 //  json.go
 //
-//  Copyright (c) 2016, Jared Chavez. 
+//  Copyright (c) 2016, Jared Chavez.
 //  All rights reserved.
 //
 //  Use of this source code is governed by a BSD-style
@@ -17,8 +17,8 @@ package json
 import (
     "encoding/json"
     "fmt"
-    "strings"
     "strconv"
+    "strings"
 )
 
 // Element type enumeration.
@@ -28,10 +28,10 @@ const (
     TypeValue
 )
 
-var typeLookup = map[int]string {
-    0 : "Object",
-    1 : "Array",
-    2 : "Value",
+var typeLookup = map[int]string{
+    0: "Object",
+    1: "Array",
+    2: "Value",
 }
 
 // EmptyElement represents an empty JSON Element of any type
@@ -69,20 +69,20 @@ func Delete(e Element, path string) error {
 
 // Parse reads a block of JSON data and attempts to parse it
 // into a JSON Element object and sub-objects.
-func Parse(data []byte) Element {
+func Parse(data []byte) (Element, error) {
     var jsonData interface{}
 
     err := json.Unmarshal(data, &jsonData)
     if err != nil {
-        return EmptyElement
+        return EmptyElement, err
     }
 
     obj, err := newItem(nil, "", jsonData)
     if err != nil {
-        return EmptyElement
+        return EmptyElement, err
     }
 
-    return obj
+    return obj, nil
 }
 
 // Search allows you to search through the given JSON element
@@ -91,11 +91,11 @@ func Parse(data []byte) Element {
 // Ex: myobject.myarray[1].myvalue
 func Search(e Element, path string) Element {
     curVal := e
-    
+
     pathParts := strings.Split(path, ".")
     for i := range pathParts {
         key, idxList := parsePathPart(pathParts[i])
-        
+
         newVal, err := get(curVal, key)
         if err != nil {
             return EmptyElement
@@ -153,20 +153,20 @@ func deleteElement(parent Element, child Element) {
 // get attempts to retrieve a child of the given element by key name.
 func get(e Element, key string) (Element, error) {
     switch obj := e.(type) {
-        case *Object:
-            val, ok := obj.children[key]
-            if !ok {
-                return nil, fmt.Errorf("Key missing (%s)", key)
-            }
+    case *Object:
+        val, ok := obj.children[key]
+        if !ok {
+            return nil, fmt.Errorf("Key missing (%s)", key)
+        }
 
-            return val, nil
-        case *Array:
-            return nil, fmt.Errorf("Array type does not support get by key")
-        case *Value:
-            return nil, fmt.Errorf("Value does not support get by key")
-        default:
-            err := fmt.Errorf("get : Invalid type (%v)", obj)
-            return nil, err
+        return val, nil
+    case *Array:
+        return nil, fmt.Errorf("Array type does not support get by key")
+    case *Value:
+        return nil, fmt.Errorf("Value does not support get by key")
+    default:
+        err := fmt.Errorf("get : Invalid type (%v)", obj)
+        return nil, err
     }
 
     err := fmt.Errorf("get unknown error")
@@ -176,28 +176,28 @@ func get(e Element, key string) (Element, error) {
 // getIdx attempts to retrieve a child of the given elemnent by array index.
 func getIdx(e Element, idx int) (Element, error) {
     switch obj := e.(type) {
-        case *Object:
-            return nil, fmt.Errorf("GetIdx not implemented on object type")
-        case *Array:
-            if idx < 0 {
-                return nil, fmt.Errorf("Invalid index (%d)", idx)
-            }
+    case *Object:
+        return nil, fmt.Errorf("GetIdx not implemented on object type")
+    case *Array:
+        if idx < 0 {
+            return nil, fmt.Errorf("Invalid index (%d)", idx)
+        }
 
-            if idx >= len(obj.children) {
-                err := fmt.Errorf(
-                    "Idx outside array bounds (%d >= %d)", 
-                    idx, 
-                    len(obj.children),
-                )
-                
-                return nil, err
-            }
-            return obj.children[idx], nil
-        case *Value:
-            return nil, fmt.Errorf("Value does not support get by idx")
-        default:
-            err := fmt.Errorf("getIdx : Invalid type (%v)", obj)
+        if idx >= len(obj.children) {
+            err := fmt.Errorf(
+                "Idx outside array bounds (%d >= %d)",
+                idx,
+                len(obj.children),
+            )
+
             return nil, err
+        }
+        return obj.children[idx], nil
+    case *Value:
+        return nil, fmt.Errorf("Value does not support get by idx")
+    default:
+        err := fmt.Errorf("getIdx : Invalid type (%v)", obj)
+        return nil, err
     }
 
     err := fmt.Errorf("getIdx unknown error")
@@ -205,7 +205,7 @@ func getIdx(e Element, idx int) (Element, error) {
 }
 
 // newItem recursively builds up a new JSON Element with the given name and parent
-// from the provided data. Simple values are converted into Value elements. 
+// from the provided data. Simple values are converted into Value elements.
 // map[string]interface{} data value are converted to JSON Objects. interface{}
 // arrays are converted into Array elements.
 func newItem(parent Element, name string, data interface{}) (Element, error) {
@@ -240,16 +240,16 @@ func newItem(parent Element, name string, data interface{}) (Element, error) {
 // parsePathPart takes a given path element and attempts to parse it
 // for key names and array indexes.
 func parsePathPart(item string) (string, []int) {
-    key      := ""
+    key := ""
     startIdx := 0
-    idxList  := make([]int, 0)
+    idxList := make([]int, 0)
 
     startIdx = strings.Index(item, "[")
-    
+
     if startIdx == -1 {
         return item, idxList
     }
-    
+
     key = item[:startIdx]
 
     for {
@@ -259,13 +259,13 @@ func parsePathPart(item string) (string, []int) {
             return key, idxList
         }
 
-        idx, err := strconv.ParseInt(item[startIdx + 1:endIdx], 10, 32)
+        idx, err := strconv.ParseInt(item[startIdx+1:endIdx], 10, 32)
         if err != nil {
             return key, idxList
         }
 
         idxList = append(idxList, int(idx))
-        tmp    := strings.Index(item[endIdx:], "[")
+        tmp := strings.Index(item[endIdx:], "[")
 
         if tmp == -1 {
             return key, idxList
